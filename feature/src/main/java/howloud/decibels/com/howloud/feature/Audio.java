6869faa18bd59b1ded2b8e9c28512755c88d355e
halyngoc <halyngoc@users.noreply.github.com>
2018-04-21 10:50:03 -0700

    Add class Audio with dummy getVolume

---
+++ feature/src/main/java/howloud/decibels/com/howloud/feature/Audio.java
@@ -0,0 +1,34 @@
+package howloud.decibels.com.howloud.feature;
+
+import android.media.MediaRecorder;
+
+import java.io.IOException;
+
+public final class Audio {
+    private MediaRecorder recorder = new MediaRecorder();
+
+    public Audio() throws IOException {
+        recorder.prepare();
+    }
+
+    public void start() {
+        recorder.start();
+    }
+
+    public void stop() {
+        recorder.stop();
+    }
+
+    public void release(){
+        recorder.release();
+    }
+
+    private int getMaximumAmplitude() {
+        return recorder.getMaxAmplitude();
+    }
+
+    // dummy function
+    public double getVolume() {
+        return 99999;
+    }
+}
