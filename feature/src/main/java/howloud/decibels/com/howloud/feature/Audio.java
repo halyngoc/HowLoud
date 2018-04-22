44ebd65ea3d7faf3350ca12fa9b30aaf7326fbf3
Ha Ly <lyha@pdx.edu>
2018-04-22 16:07:28 -0700

    Fix getVolume's formula; Remove comments

--- feature/src/main/java/howloud/decibels/com/howloud/feature/Audio.java
+++ feature/src/main/java/howloud/decibels/com/howloud/feature/Audio.java
@@ -13,8 +13,7 @@ public final class Audio {
         recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
         recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
         recorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
-        //File temp = File.createTempFile("dec", "dec");
-        recorder.setOutputFile("/dev/null");//temp.getAbsolutePath());
+        recorder.setOutputFile("/dev/null");
         recorder.prepare();
     }
 
@@ -33,18 +32,8 @@ public final class Audio {
         recorder.release();
     }
 
-    //public void ondataavailable() {
- //       recorder.ondataavailable();
-  //  }
-
-//    private int getMaximumAmplitude() {
-//        return recorder.getMaxAmplitude();
-//    }
-
-
-    // not sure about the 2700.0 thing but I found it on stackoverflow
     public double getVolume() {
-//        return 20* Math.log10(recorder.getMaxAmplitude() / 2700.0);
-        return recorder.getMaxAmplitude();
+        return 20 * Math.log10(recorder.getMaxAmplitude() / 2700.0);
+//        return recorder.getMaxAmplitude();
     }
 }
